fe0e1aab3d5412079e530b0d89e157a4169bdba9
Avinash Som <avinash.som17@gmail.com>
2026-07-23 21:42:54 +0530

    chatApp

---
+++ events/chatApp/index.js
@@ -0,0 +1,24 @@
+const ChatRoom = require("./chatRoom.js")
+
+const chat = new ChatRoom();
+
+chat.on('join', (user) => {
+    console.log(`${user} has joined the chat`)
+})
+chat.on('message', (user, message) => {
+    console.log(`${user} : ${message}`)
+})
+chat.on('leave', (user) => {
+    console.log(`${user} has left the chat`)
+})
+
+//simulating the chat
+chat.join('Alice')
+chat.join('Bob')
+
+chat.sendMessage('Alice' , 'hey Bob, hello to everyone ')
+chat.sendMessage('Bob' , 'hey Alice, hello to everyone ')
+
+chat.leave('Alice')
+chat.sendMessage('Alice', "this is not run/send")
+chat.leave('Bob')
